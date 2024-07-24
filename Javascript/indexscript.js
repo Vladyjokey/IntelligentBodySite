8a63eaf558d6327453830219ef790654f58dce4f
Vladyjokey <92403896+Vladyjokey@users.noreply.github.com>
2024-07-24 18:39:22 +0300

    a

--- Javascript/indexscript.js
+++ Javascript/indexscript.js
@@ -1,20 +1,5 @@
-let navLinks = document.getElementById("navLinks")
-let fa = document.getElementById("faTimes")
-
 // page loading animations
 document.addEventListener("DOMContentLoaded", function() {
-    // Navigation bar color on load
-    const scrollPosition = window.scrollY;
-    const maxScroll = 500; // Adjust this value based on when you want the color to be fully opaque
-
-    if (scrollPosition > 0) {
-        const opacity = Math.min(scrollPosition / maxScroll, 1); // Calculate opacity based on scroll position
-        navLinks.style.background = `linear-gradient(to bottom, rgba(99, 158, 61, ${opacity}) 75%, rgba(99, 158, 61, ${opacity - 0.5}) 100%)`;
-    } else {
-        navLinks.style.backgroundColor = "rgba(99, 158, 61, 0)"; // Fully transparent
-    }
-
-
     let infoBox = document.querySelector(".infoBox")
     infoBox.classList.add("animate")
     let description = document.querySelector(".description")
@@ -46,18 +31,6 @@ document.addEventListener("DOMContentLoaded", function() {
     sBoxes.forEach(function(sBox) {
         observer.observe(sBox);
     });
-});
-
-window.addEventListener('scroll', function() {
-    const scrollPosition = window.scrollY;
-    const maxScroll = 500; // Adjust this value based on when you want the color to be fully opaque
-
-    if (scrollPosition > 0) {
-        const opacity = Math.min(scrollPosition / maxScroll, 1);
-        navLinks.style.background = `linear-gradient(to bottom, rgba(99, 158, 61, ${opacity}) 75%, rgba(99, 158, 61, ${opacity - 0.5}) 100%)`;
-    } else {
-        navLinks.style.background = "rgba(99, 158, 61, 0)";
-    }
 });
 
 let p = document.getElementById("contactBox")
@@ -93,14 +66,16 @@ function copyToClipboard(text) {
     document.body.removeChild(textArea);
 }
 
-function showMenu(){
-    navLinks.style.right = "0"
-    fa.style.top = "0"
-    fa.style.right = "0"
+function showMenu() {
+    if(document.getElementById("navLinks").classList.contains("show"))
+        document.getElementById("navLinks").classList.remove("show");
+    else
+        document.getElementById("navLinks").classList.add("show");
+
+    console.log(document.getElementById("navLinks").classList)
 }
 
-function hideMenu(){
-    navLinks.style.right = "-200px"
-    fa.style.top = "-8vh"
-    fa.style.right = "-2vh"
+function hideMenu() {
+    document.getElementById("navLinks").classList.remove("show");
+    console.log('ththsthshtsh')
 }
